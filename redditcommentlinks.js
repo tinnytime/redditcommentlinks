5f0c5b6c95818e263609c5a9cbe2b758b531ac96
Tinny <cnguyen.biz@gmail.com>
2020-09-03 15:39:28 +1000

    Fix unique href

--- redditcommentlinks.js
+++ redditcommentlinks.js
@@ -7,13 +7,14 @@ listWrapper.appendChild(list)
 var unique = []
 
 for (let _link of links) {
+    href = _link.href.toLowerCase()
     // Dedupe
-    if (unique.indexOf(_link.href) != -1) continue
-    unique.push(_link.href)
+    if (unique.indexOf(href) != -1) continue
+    unique.push(href)
 
     // Don't show user links
     let u = /https?:\/\/.*\.reddit\.com\/(u|message)\//
-    if (u.exec(_link.href)) continue
+    if (u.exec(href)) continue
 
     let item = document.createElement('li')
     let link = _link.cloneNode(true)
